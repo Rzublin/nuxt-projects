01a8baa2323cce5ed6a49d6c0af37bdc37776feb
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-04-18 19:08:31 +0200

    add api call

--- pages/event/_id.vue
+++ pages/event/_id.vue
@@ -1,11 +1,27 @@
 <template>
   <div>
-    <h1>Event #{{ id }}</h1>
+    <h1>{{ event.title }}</h1>
   </div>
 </template>
 
 <script>
 export default {
+  async asyncData({ $axios, error, params }) {
+    try {
+      const { data } = await $axios.get(
+        'https://my-json-server.typicode.com/Rzublin/placeholder-db/events/' +
+          params.id
+      )
+      return {
+        event: data
+      }
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: `Unable to fetch event #${params.id} at this time. Please try again later.`
+      })
+    }
+  },
   data() {
     return {
       id: this.$route.params.id
@@ -13,12 +29,12 @@ export default {
   },
   head() {
     return {
-      title: `Event #${this.id}`,
+      title: `Event ${this.event.title}`,
       meta: [
         {
           hid: 'description',
           name: 'description',
-          content: 'All the information about the event ' + this.id
+          content: 'All the information about the event ' + this.event.title
         }
       ]
     }
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,30 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
+import EventCard from '../components/EventCard.vue'
 export default {
+  components: { EventCard },
+
+  async asyncData({ $axios, error }) {
+    try {
+      const { data } = await $axios.get(
+        'https://my-json-server.typicode.com/Rzublin/placeholder-db/events'
+      )
+      return {
+        events: data
+      }
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch events at this time. Please try again later.'
+      })
+    }
+  },
   head() {
     return {
       title: 'List Events',
